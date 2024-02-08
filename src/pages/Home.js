a6e623067538bf53960c19c066386dd8a1eaac4c
Ian July <ianjuly4@gmail.com>
2024-02-07 18:55:10 -0700

    route v6 lab

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,29 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard";
 
 function Home() {
+  const [movies, setMovies] = useState([])
+  useEffect(()=>{
+    fetch(" http://localhost:4000/movies")
+      .then((r)=>r.json())
+      .then((movieData)=>setMovies(movieData))
+      .catch((error) => console.error("Error fetching movies:", error));
+  },[])
+  console.log(movies)
+
+  
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar/>
       </header>
       <main>
-        {/* Info goes here! */}
+       <h1>Home Page</h1>
+       {movies.map((movie) =>{
+        return <MovieCard key={movie.id} title={movie.title} id={movie.id} />
+      })
+    }
       </main>
     </>
   );
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,33 @@
-import { useEffect, useState } from "react";
+
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import { useParams } from 'react-router-dom';
 
 function Movie() {
+  const [movie, setMovie] = useState({});
+  const params = useParams();
+  const movieId = params.id;
+
+useEffect(() =>{
+  fetch(`http://localhost:4000/movies/${movieId}`)
+  .then(r => r.json())
+  .then(data => setMovie(data))
+  .catch(error => console.error(error))
+}, [movieId]);
+
+const movieGenres = movie.genres || [];
+const genres = movieGenres.map((genre) => {
+  return <span key={genre}>{genre}</span>;
+});
   return (
     <>
       <header>
-        {/* What component should go here? */}
+      <NavBar/>
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>TIME: {movie.time}</p>
+        {genres}
       </main>
     </>
   );
